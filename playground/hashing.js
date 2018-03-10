9c8140fb5cba8ac931fb86b2761e87d4fb9e7187
Linquas <steven84059@gmail.com>
2018-03-10 17:34:23 +0800

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+// var SHA256 = crypto.createHash('sha256');
+
+var data = {
+    id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+
+// var message = 'I am user number 3';
+
+// // SHA256.update(message);
+// // var hash2 = SHA256.digest('hex');
+
+// var hash = crypto.createHash('sha256').update(message).digest('hex');
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+// var token = {
+//     data,
+//     hash: crypto.createHash('sha256').update(JSON.stringify(data) + 'somesrcret').digest('hex')
+// }
+
+// // token.data.id = 5;
+// // token.hash = crypto.createHash('sha256').update(JSON.stringify(data)).digest('hex');
+
+// var resultHash = crypto.createHash('sha256').update(JSON.stringify(data) + 'somesrcret').digest('hex')
+
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed')
+// } else {
+//     console.log('Data was changed');
+// }
+
