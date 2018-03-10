aad4fe890eb48d27eaef9b99557899c2390250cb
Linquas <steven84059@gmail.com>
2018-03-10 11:39:59 +0800

    Test the DELET /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,43 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos:/id', () => {
+    it('should remove a todo', (done) => {
+        var herID = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${herID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(herID);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(herID).then((todo) => {
+                    expect(todo).toBeFalsy();
+                    done();
+                }).catch((e) => {
+                    done(e);
+                })
+            })
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object ID is invalid', (done) => {
+        request(app)
+            .delete(`/todos/123asd`)
+            .expect(404)
+            .end(done);
+    });
+})
